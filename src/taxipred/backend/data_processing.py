31ecb0f563a72ff80b776070bc07b845c2658f91
glennart8 <henrikpilback@gmail.com>
2025-09-19 13:08:52 +0200

    Created a pydantic TaxiData-class containg the attributes which the dataset is build of.
    Also made method predict_price() which converts the datetime-objekts into string values based on time.

--- src/taxipred/backend/data_processing.py
+++ src/taxipred/backend/data_processing.py
@@ -1,9 +1,21 @@
 from taxipred.utils.constants import TAXI_CSV_PATH
 import pandas as pd
 import json
+from datetime import datetime 
+from pydantic import BaseModel, Field
 
-
-class TaxiData:
+# Behöver jag en klass för att träna modellen och en klass för att skicka in datan
+# som simuleras med streamlit, den innehåller ju inte flera av de värden som taxidata har
+class TaxiData(BaseModel):
+    Trip_Distance_km: float = Field(gt=0) 
+    Time_of_Day: str
+    Day_of_Week: str
+    Base_Fare: float = Field(gt=0)
+    Per_Km_Rate: float = Field(gt=0)
+    Per_Minute_Rate: float = Field(gt=0)
+    Trip_Duration_Minutes: float = Field(gt=0)
+    Trip_Price: float = Field(gt=0)
+    
     def __init__(self):
         self.df = pd.read_csv(TAXI_CSV_PATH)
 
@@ -21,9 +33,32 @@ class TaxiData:
             
         return trip_data
  
-    def search_trip():
-        pass
+    def predict_price(self, distance_km: float, trip_duration_minutes: float, trip_datetime: datetime):
 
-
-
-# Ska jag använda pydantic?! Varför när den redan har gåtts igenom den?
+        # Konvertera datetime-värdena till strings för att kunna mappa mot datasetet
+        hour = trip_datetime.hour
+        if 5 <= hour < 12:
+            time_of_day = "Morning"
+        elif 12 <= hour < 17:
+            time_of_day = "Afternoon"
+        elif 17 <= hour < 22:
+            time_of_day = "Evening"
+        else:
+            time_of_day = "Night"
+        
+        # Ta ut endast veckodag (vet inte om den kan matcha engelska)
+        day_of_week = trip_datetime.strftime('%A')
+        
+        input_data = pd.DataFrame([[
+            distance_km,
+            trip_duration_minutes,
+            time_of_day,
+            day_of_week
+        ]], columns=['Trip_Distance_km', 'Trip_Duration_Minutes', 'Time_of_Day', 'Day_of_Week'])
+        
+        # -- Anropa modell --- 
+        
+        # if self.model:
+        #     predicted_price = self.model.predict(input_data)[0]
+        #     return {"predicted_price": round(predicted_price, 2)}
+        
