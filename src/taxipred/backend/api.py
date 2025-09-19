a291d763fd4645a71432fad9a82d4d6d6e5f0b47
glennart8 <henrikpilback@gmail.com>
2025-09-19 13:07:37 +0200

    Started a PredictRequest-class containing the values from streamlit which will later on be processed by the model

--- src/taxipred/backend/api.py
+++ src/taxipred/backend/api.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from taxipred.backend.data_processing import TaxiData
+from pydantic import BaseModel
+from datetime import datetime
+
+class PredictRequest(BaseModel):
+    distance_km: float
+    trip_duration_minutes: float
+    trip_datetime: datetime # FastAPI/Pydantic konverterar ISO-strängen till ett datetime-objekt
+    # Base_Fare
+    # Per_Km_Rate
+    # Per_Minute_Rate
+    # Trip_Price
 
 app = FastAPI()
-
-# taxi_data = TaxiData()
 
 @app.get("/taxi/")
 async def read_taxi_data():
@@ -15,7 +24,17 @@ async def read_avg_price():
     taxi_data = TaxiData()
     return taxi_data.avg_price()
 
-@app.get("/taxi/search_trip/")
-async def search_trip():
+# @app.get("/taxi/search_trip/")
+# async def search_trip():
+#     taxi_data = TaxiData()
+#     return taxi_data.search_trip()
+
+@app.post("/predict_price/")
+async def predict_price(request: PredictRequest):
+    # Anropa predict_price-metoden och skicka med datetime-objektet
     taxi_data = TaxiData()
-    return taxi_data.search_trip()
+    return taxi_data.predict_price(
+        distance_km=request.distance_km,
+        trip_duration_minutes=request.trip_duration_minutes,
+        trip_datetime=request.trip_datetime
+    )
