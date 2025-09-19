c2c88d3698829d17beca7b7169f0cb6250575906
glennart8 <henrikpilback@gmail.com>
2025-09-19 12:26:25 +0200

    Made a post_api_endpoint in helpers.py

--- src/taxipred/utils/helpers.py
+++ src/taxipred/utils/helpers.py
@@ -1,5 +1,6 @@
 import requests 
 from urllib.parse import urljoin
+import json
 
 def read_api_endpoint(endpoint = "/", base_url = "http://127.0.0.1:8000"):
     url = urljoin(base_url, endpoint)
@@ -7,5 +8,14 @@ def read_api_endpoint(endpoint = "/", base_url = "http://127.0.0.1:8000"):
     
     return response
 
-# TODO:
-# post_api_endpoint
+
+def post_api_endpoint(endpoint, data, base_url="http://127.0.0.1:8000"):
+    url = urljoin(base_url, endpoint)
+    headers = {'Content-Type': 'application/json'} # Datan som skickas är i JSON-format
+    try:
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+        response.raise_for_status()
+        return response
+    except requests.exceptions.RequestException as e:
+        print(f"Error making POST request to {url}: {e}")
+        return None
