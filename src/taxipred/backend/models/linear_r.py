60857f9f01876bdd8b41b5ae2a5826fcac40f275
glennart8 <henrikpilback@gmail.com>
2025-09-22 09:29:55 +0200

    Moved the code from ipynb-file to .py-file, creating the .pkl-model files

--- src/taxipred/backend/models/linear_r.py
+++ src/taxipred/backend/models/linear_r.py
@@ -1,8 +1,44 @@
-from taxipred.utils.constants import TAXI_CSV_PATH
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+import joblib
+from taxipred.utils.constants import TAXI_CSV_PATH
 
 df = pd.read_csv(TAXI_CSV_PATH)
-print(df)
 
+# Ta bort kolumner som inte används
+df = df.drop(columns="Day_of_Week")
+df = df.dropna(subset=["Weather"])
 
+# Dummy encoding för kategoriska variabler
+df = pd.get_dummies(
+    df,
+    columns=['Time_of_Day', "Weather"],
+    dtype=int
+)
+
+# --- DEFINIERA FEATURES OCH TARGET ---
+X = df.drop("Trip_Price", axis="columns")
+y = df["Trip_Price"]
+
+# --- TRAIN-TEST SPLIT ---
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=42
+)
+
+# --- SKALNING ---
+scaler = MinMaxScaler()
+scaler.fit(X_train)
+
+scaled_X_train = scaler.transform(X_train)
+scaled_X_test = scaler.transform(X_test)
+
+# --- MODELLTRÄNING ---
+model = LinearRegression()
+model.fit(scaled_X_train, y_train)
+
+# --- SPARA MODELL OCH SCALER ---
+# Måste göras för att kunna använda dem i din API senare
+joblib.dump(model, 'linear_regression_model.pkl')
+joblib.dump(scaler, 'scaler.pkl')
