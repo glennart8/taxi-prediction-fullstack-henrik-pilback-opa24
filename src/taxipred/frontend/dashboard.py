379daca65a92437efa9351beb7b1e5754e377e9b
glennart8 <henrikpilback@gmail.com>
2025-09-25 10:13:21 +0200

    Changed the structure of columns in streamlit, now much easier to read

--- src/taxipred/frontend/dashboard.py
+++ src/taxipred/frontend/dashboard.py
@@ -11,7 +11,6 @@ import json
 
 # --- Setup ---
 load_dotenv()
-
 GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY")
 gmaps = googlemaps.Client(key=GOOGLE_MAPS_API_KEY)
 geolocator = Nominatim(user_agent="taxi_app")
@@ -22,7 +21,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://images.unsplash.com/photo-1565531152238-5f20a0f4a3f0?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url("https://images.unsplash.com/photo-1565531152238-5f20a0f4a3f0?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0");
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
@@ -32,151 +31,97 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# --- Input & KPI:er ---
 
-st.markdown("# Uppskatta taxiresa")
-st.markdown("### Beräkna pris för din resa")
+col_left, col_right = st.columns([3, 2])
 
-col1, col2, col3 = st.columns([1, 2, 2])
-
-with col1:
+with col_left:
+    st.title("Uppskatta pris på taxiresa")
+    st.header("Beräkna pris för din resa")
     start_address = st.text_input("Från:")
     end_address = st.text_input("Till:")
 
-if st.button("Uppskatta pris"):
-    if start_address and end_address:
-        # geopy för att hämta koordinater
-        try:
-            start_location = geolocator.geocode(start_address)
-            end_location = geolocator.geocode(end_address)
+    if st.button("Uppskatta pris"):
+        if start_address and end_address:
+            try:
+                start_location = geolocator.geocode(start_address)
+                end_location = geolocator.geocode(end_address)
 
-            if start_location and end_location:
-                # Google Maps för att beräkna körsträckan och tid
-                directions_result = gmaps.directions(
-                    start_location.point,
-                    end_location.point,
-                    mode="driving"
-                )
+                if start_location and end_location:
+                    directions_result = gmaps.directions(
+                        start_location.point,
+                        end_location.point,
+                        mode="driving"
+                    )
 
-                distance_km = directions_result[0]['legs'][0]['distance']['value'] / 1000
-                duration_minutes = directions_result[0]['legs'][0]['duration']['value'] / 60
+                    distance_km = directions_result[0]['legs'][0]['distance']['value'] / 1000
+                    duration_minutes = directions_result[0]['legs'][0]['duration']['value'] / 60
 
-                with col2:
-                    st.write("")
-                    st.success(f"Avståndet för din resa är: {distance_km:.2f} km")
-                    st.write("")
-                    st.success(f"Restiden är: {duration_minutes:.0f} minuter")
-                
-                current_time = datetime.now()
+                    st.success(f"Avstånd: {distance_km:.2f} km")
+                    st.success(f"Restid: {duration_minutes:.0f} minuter")
 
-                input_data = {
-                    "distance_km": distance_km,
-                    "trip_duration_minutes": duration_minutes,
-                    "trip_datetime": current_time.isoformat() 
-                }
-                
-                # Anropa POST-endpoint i backend
-                response = post_api_endpoint(endpoint="/predict_price/", data=input_data)
-                
-                # Sparar api-responsen i en variabel och skriver ut den
-                predicted_prices = response.json()
-                
-                # Hämta priserna från den uppdaterade JSON-strukturen
-                predicted_price_lr = predicted_prices.get("predicted_price_lr", 0)
-                predicted_price_rf = predicted_prices.get("predicted_price_rf", 0)
-                
-                # Omvandla till svenska kr
-                price_predicted_sek_lr = predicted_price_lr * 9.35
-                price_predicted_sek_rf = predicted_price_rf * 9.35
-                
-                # --- Visa pris ----
-                col1, col2, col3, col4 = st.columns(4)
+                    current_time = datetime.now()
+                    input_data = {
+                        "distance_km": distance_km,
+                        "trip_duration_minutes": duration_minutes,
+                        "trip_datetime": current_time.isoformat()
+                    }
 
-                with col1:
-                    st.metric(label="Uppskattat pris (Linear Regression)", value=f"{price_predicted_sek_lr:.2f} kr")
+                    response = post_api_endpoint(endpoint="/predict_price/", data=input_data)
+                    predicted_prices = response.json()
+                    predicted_price_lr = predicted_prices.get("predicted_price_lr", 0)
+                    predicted_price_rf = predicted_prices.get("predicted_price_rf", 0)
+                    price_predicted_sek_lr = predicted_price_lr * 9.35
+                    price_predicted_sek_rf = predicted_price_rf * 9.35
 
-                with col2:
-                    st.metric(label="Uppskattat pris (Random Forest)", value=f"{price_predicted_sek_rf:.2f} kr")
-                    
-                with col3:
-                    st.write("")
-                    
-                with col4:
-                    st.write("")
-                 
-            else:
-                st.error("Kunde inte hitta en eller båda adresserna. Vänligen försök igen.")
-        
-        except Exception as e:
-            st.error(f"Ett fel uppstod: {e}")
-    else:
-        st.error("Fyll i både från- och till-adresserna.")
+                    st.metric("Uppskattat pris (Linear Regression)", f"{price_predicted_sek_lr:.2f} kr")
+                    st.metric("Uppskattat pris (Random Forest)", f"{price_predicted_sek_rf:.2f} kr")
 
-
-data = read_api_endpoint("taxi")
-if data:
-    df = pd.DataFrame(data.json())
-    col_1, col_2, col_kpis = st.columns([3, 3, 2])
-    
-    with col_1:
-        st.markdown("### Resedata")
-        st.dataframe(df)
-        
-    with col_2:
-        st.markdown("### PLOTS")
-
-        response = read_api_endpoint("/taxi/distribution_plot")
-
-        if response.status_code == 200:
-            fig_json = response.json()            #  får str från API
-            fig = go.Figure(json.loads(fig_json)) #  konvertera str till dict
-            st.plotly_chart(fig)
+                else:
+                    st.error("Kunde inte hitta adresserna.")
+            except Exception as e:
+                st.error(f"Ett fel uppstod: {e}")
         else:
-            st.error(f"Fel från API: {response.status_code}")
-        
-    with col_kpis:
-        st.markdown("### KPI:s")
-        avg_prices = read_api_endpoint("taxi/avg_price/")         
-        avg_prices_data = avg_prices.json()
-        sek = 9.35
-        
-        price_sek_10 = avg_prices_data['10'] * sek
-        price_sek_20 = avg_prices_data['20'] * sek
-        price_sek_30 = avg_prices_data['30'] * sek
-        
-        st.metric(label="Medelpris (< 10 min)", value=f"{price_sek_10:.2f} :-")
-        st.metric(label="Medelpris (< 20 min)", value=f"{price_sek_20:.2f} :-")
-        st.metric(label="Medelpris (< 30 min)", value=f"{price_sek_30:.2f} :-")
-        
-        # Dyrast resa
-        most_expensive = read_api_endpoint("taxi/most_expensive")
-        most_expensive_trip = most_expensive.json()
-        
-        #Känns fel, men ett enkelt sätt, tar inte hänsyn till de andra
-        if most_expensive_trip == "Afternoon":
-            value="Eftermiddag"
-        if most_expensive_trip == "Morning":
-            value="Morgon"
-        if most_expensive_trip == "Evening":
-            value="Kväll"
-        if most_expensive_trip == "Night":
-            value="Natt"
-        
-        st.metric(label="Tidpunkt för dyrast resa: ", value=value)
-       
-        
-        # Tidpunkt då flest åker
-        most_customers = read_api_endpoint("/taxi/most_customers")
-        most_customers_time_of_day = most_customers.json()
-        
-        # Kanske fel sätt, men gör de andra if-satserna också
-        if most_customers_time_of_day == "Afternoon":
-            value="Eftermiddag"
-        if most_customers_time_of_day == "Morning":
-            value="Morgon"
-        if most_customers_time_of_day == "Evening":
-            value="Kväll"
-        if most_customers_time_of_day == "Night":
-            value="Natt"
-        st.metric(label="Flest resenärer: ", value=value)
+            st.error("Fyll i både från- och till-adresserna.")
 
-        
+with col_right:
+    st.header("KPI:s")
+    avg_prices = read_api_endpoint("taxi/avg_price/").json()
+    sek = 9.35
+    
+    col_kpi_left, col_kpi_right = st.columns(2)
+    
+    with col_kpi_left:
+        st.metric("Medelpris (<10 min)", f"{avg_prices['10']*sek:.2f} :-")
+        st.metric("Medelpris (<20 min)", f"{avg_prices['20']*sek:.2f} :-")
+        st.metric("Medelpris (<30 min)", f"{avg_prices['30']*sek:.2f} :-")
+
+    with col_kpi_right:
+        # Skapar dicten mapping för att mappa engelska namn med svenska 
+        mapping = {"Afternoon": "Eftermiddag", "Morning": "Morgon", "Evening": "Kväll", "Night": "Natt"}
+        most_expensive = read_api_endpoint("taxi/most_expensive").json()
+        st.metric("Tidpunkt för dyrast resa", mapping.get(most_expensive, most_expensive))
+
+        most_customers = read_api_endpoint("/taxi/most_customers").json()
+        st.metric("Flest resenärer", mapping.get(most_customers, most_customers))
+
+# ---Resedata & Plot ---
+
+col_data, col_plot = st.columns([3, 3])
+
+with col_data:
+    st.header("Resedata")
+    data = read_api_endpoint("taxi")
+    if data:
+        df = pd.DataFrame(data.json())
+        st.dataframe(df)
+
+with col_plot:
+    st.header("Fördelning av resor per tid på dygnet")
+    response = read_api_endpoint("/taxi/distribution_plot")
+    if response.status_code == 200:
+        fig_json = response.json()
+        fig = go.Figure(json.loads(fig_json))
+        st.plotly_chart(fig, use_container_width=True)
+    else:
+        st.error(f"Fel från API: {response.status_code}")
